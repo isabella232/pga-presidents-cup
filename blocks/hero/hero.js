23d8bbf06a6afaa99bae9d7e669910d933a74f29
Katie Sexton <ksexton@adobe.com>
2022-07-22 15:55:01 -0600

    fix: style video hero more universally

--- blocks/hero/hero.js
+++ blocks/hero/hero.js
@@ -3,6 +3,7 @@ import { decorateBlock, loadBlock } from '../../scripts/scripts.js';
 export default async function decorate(block) {
   const video = block.querySelector('.video');
   if (video) {
+    block.classList.add('hero-video');
     video.classList.add(`${video.className}-hero`);
     decorateBlock(video);
     await loadBlock(video);
