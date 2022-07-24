18fc71b17c8851435a2642b8b6e1ad7a39a852fe
Katie Sexton <ksexton@adobe.com>
2022-07-23 22:33:24 -0600

    feat: course carousel

--- blocks/carousel/carousel.js
+++ blocks/carousel/carousel.js
@@ -1,11 +1,40 @@
 export default function decorate(block) {
+  const blockClasses = [...block.classList];
   const buttons = document.createElement('div');
   buttons.className = 'carousel-buttons';
+  if (blockClasses.includes('course')) buttons.classList.add('course-buttons');
   [...block.children].forEach((row, i) => {
     const classes = ['image', 'text'];
     classes.forEach((e, j) => {
       if (row.children[j]) row.children[j].classList.add(`carousel-${e}`);
     });
+    /* course carousel */
+    if (blockClasses.includes('course')) {
+      const text = row.querySelector('.carousel-text');
+      text.classList.add('course-text');
+      // setup overview (title, img, desc)
+      const overview = document.createElement('div');
+      overview.classList.add('course-overview');
+      overview.append(
+        text.querySelector('h2'), // title
+        text.querySelector('h2 + h3'), // par heading
+        text.querySelector('h2 + h3 + p'), // course img
+        text.querySelector('h2 + h3 + p + p'), // course desc
+      );
+      const holeImg = overview.querySelector('picture');
+      if (holeImg) holeImg.classList.add('course-hole');
+      // setup stats
+      const stats = document.createElement('div');
+      stats.classList.add('course-stats');
+      stats.append(
+        text.querySelector('h3'), // stats heading
+        text.querySelector('h3 + ul'), // stats list
+      );
+      text.prepend(overview, stats);
+      // setup photo credits
+      const credits = text.querySelector('p > em');
+      if (credits) credits.parentNode.classList.add('course-credits');
+    }
     /* buttons */
     const button = document.createElement('button');
     if (!i) button.classList.add('selected');
@@ -16,5 +45,5 @@ export default function decorate(block) {
     });
     buttons.append(button);
   });
-  block.parentElement.append(buttons);
+  block.parentElement.prepend(buttons);
 }
