e98dc5f50a2ab200cb0b8847f4d43ac32859974e
Katie Sexton <ksexton@adobe.com>
2022-07-27 09:27:14 -0600

    feat: filter news block by tag

--- blocks/news/news.js
+++ blocks/news/news.js
@@ -63,7 +63,12 @@ export default async function decorate(block) {
   block.textContent = '';
   // populate news content
   /* TODO: add CORS header, to be replaced with direct API */
-  const directURL = `${newsURL}/lang=LANG_NOT_DEFINED&path=/content&tags=PGATOUR:Tournaments/2018/r011+PGATOUR:Tournaments/2020/r011+PGATOUR:Tournaments/2019/r011+PGATOUR:Tournaments/2021/r011+PGATOUR:Tournaments/2022/r011&size=${limit}`;
+  let directURL;
+  if (config.tags) {
+    directURL = `${newsURL}/tags=${config.tags.replace(/ /g, '')}&size=${limit}`;
+  } else {
+    directURL = `${newsURL}/lang=LANG_NOT_DEFINED&path=/content&tags=PGATOUR:Tournaments/2018/r011+PGATOUR:Tournaments/2020/r011+PGATOUR:Tournaments/2019/r011+PGATOUR:Tournaments/2021/r011+PGATOUR:Tournaments/2022/r011&size=${limit}`;
+  }
   const resp = await fetch(`https://little-forest-58aa.david8603.workers.dev/?url=${encodeURIComponent(directURL)}`);
   const json = await resp.json();
   const ul = document.createElement('ul');
