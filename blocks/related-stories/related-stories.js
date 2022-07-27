518b43ea98920e9c824b48bf53e61b7621ddc115
Katie Sexton <ksexton@adobe.com>
2022-07-27 09:27:42 -0600

    feat: related stories block

--- blocks/related-stories/related-stories.js
+++ blocks/related-stories/related-stories.js
@@ -1,4 +1,42 @@
-export default function decorate(block) {
-  const stories = block.querySelectorAll('a');
+import { readBlockConfig } from '../../scripts/scripts.js';
+
+export default async function decorate(block) {
+  // const stories = block.querySelectorAll('a');
+  const videoPrefix = 'https://pga-tour-res.cloudinary.com/image/upload/c_fill,f_auto,g_face,h_311,q_auto,w_425/v1/';
+  const damPrefix = 'https://www.pgatour.com';
+  const config = readBlockConfig(block);
+  const storiesURL = config.source;
+  const limit = config.limit || 5;
   block.innerHTML = '';
+
+  if (storiesURL) {
+    // populate related stories content
+    /* TODO: add CORS header, to be replaced with direct API */
+    const directURL = `${storiesURL}/path=/content&tags=${config.tags.replace(/ /g, '')}&relatedTo=/content/the-players${window.location.pathname}&size=${limit}`;
+    const resp = await fetch(`https://little-forest-58aa.david8603.workers.dev/?url=${encodeURIComponent(directURL)}`);
+    const json = await resp.json();
+    const title = document.createElement('h3');
+    title.textContent = 'Related to this story';
+    const ul = document.createElement('ul');
+    json.items.forEach((story) => {
+      const prefix = story.image.startsWith('brightcove') ? videoPrefix : damPrefix;
+      const li = document.createElement('li');
+      li.classList.add('related-stories-story', `related-stories-story-${story.type}`);
+      const video = story.videoId ? '<div class="related-stories-story-play"></div>' : '';
+      const a = document.createElement('a');
+      a.href = story.link;
+      a.innerHTML = `
+        <div class="related-stories-story-image">
+          <picture><img src="${prefix}${story.image}" alt="${story.description}" / ></picture>${video}
+        </div>
+        <div class="related-stories-story-body">
+          ${story.franchise ? `<p>${story.franchise}</p>` : ''}
+          <a href="${story.link}">${story.title}</a>
+        </div>
+      `;
+      li.append(a);
+      ul.append(li);
+    });
+    block.append(title, ul);
+  }
 }
