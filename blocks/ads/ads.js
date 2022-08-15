649c11e322b12f41cef93a00a1bd36e9f12c4368
David Nuescheler <david@nuescheler.ch>
2022-08-15 13:39:17 -0600

    chore: adding ads

--- blocks/ads/ads.js
+++ blocks/ads/ads.js
@@ -1,7 +1,50 @@
 import { readBlockConfig } from '../../scripts/scripts.js';
 
+function loadScript(url, callback, type) {
+  const head = document.querySelector('head');
+  if (!head.querySelector(`script[src="${url}"]`)) {
+    const script = document.createElement('script');
+    script.src = url;
+    if (type) script.setAttribute('type', type);
+    head.append(script);
+    script.onload = callback;
+    return script;
+  }
+  return head.querySelector(`script[src="${url}"]`);
+}
+
 export default function decorate(block) {
   const config = readBlockConfig(block);
-  console.log('ads config', config);
-  block.textContent = 'Ads go here...';
+  block.innerHTML = '<div class="parsys"><div class="ad section"></div></div>';
+  const inner = block.querySelector('.ad');
+  window.pgatour = window.pgatour || {};
+  loadScript('/blocks/ads/jquery-3.6.0.min.js', () => {
+    loadScript('/blocks/ads/react-cq.min.js', () => {
+      loadScript('/blocks/ads/theme-pgatour-js-embedded.min.js', () => {
+        window.pgatour.Ad.setup(
+          {
+            site: 'pgat',
+            refreshDisabled: false,
+            trackBrowserActivity: true,
+            justInTime: true,
+            refreshOnScroll: 'none',
+            useEngageTime: true,
+            options: {
+              s1: 'pgatour',
+              s2: 'tournaments',
+              s3: 'the-players',
+              s4: 'landing',
+            },
+            enableSingleRequest: true,
+            networkCode: '9517547',
+            refreshInterval: 20,
+          },
+        );
+        // eslint-disable-next-line no-new
+        new window.pgatour.Ad(inner, {
+          trackBrowserActivity: true, options: { pos: 'leftpromo' }, refreshOnResize: false, companionAd: false, justOnScroll: false, suspended: false, size: { large: [[420, 90]], medium: [[380, 90]] },
+        });
+      });
+    });
+  });
 }
