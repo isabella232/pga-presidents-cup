066062963b0d96d593d50204d8e13cc521194d24
Katie Sexton <ksexton@adobe.com>
2022-09-02 10:06:22 -0600

    feat: extend externalize for authoring

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -176,10 +176,14 @@ function updateExternalLinks(container) {
   ];
   container.querySelectorAll('a[href]').forEach((a) => {
     try {
-      const { origin } = new URL(a.href, window.location.href);
-      if (origin && origin !== window.location.origin) {
+      const { origin, hash } = new URL(a.href, window.location.href);
+      const targetHash = hash && hash.startsWith('#_');
+      if (origin && origin !== window.location.origin && !targetHash) {
         a.setAttribute('target', '_blank');
         if (!REFERERS.includes('origin')) a.setAttribute('rel', 'noopener');
+      } else if (targetHash) {
+        a.setAttribute('target', hash.replace('#', ''));
+        a.href = a.href.replace(hash, '');
       }
     } catch (e) {
       // eslint-disable-next-line no-console
